162abd2c871347a2f0f9c1f68159d3d28c72d0b5
Muminoadedayo <Muminoadedayo@gmail.com>
2023-12-08 23:05:16 +0100

    Project added

---
+++ picaboo.js
@@ -0,0 +1,15 @@
+const age = [0, 1, 2, 3]
+console.log(age)
+
+
+const array = [
+    'Abdulmumin',
+    'Programmer',
+    27,
+    friends = ['Daniel', 'Scott','Pedro'],
+]
+
+    console.log(`My name is ${array[0]}, im a ${array[1]} by occupation. i am ${array[2]} years old.
+    my friends are ${friends.length} in number and my bestfriend is ${friends[0]}.`)
+
+
